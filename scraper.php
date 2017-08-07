ec579ea2d518e1379b0106661afdc0bdac54b2d6
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-07 17:32:07 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,27 +1,29 @@
-<?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+//
+$MAX_ID = 3; //set based on required maximum numbers
+/** looping over list of ids of doctors **/
+for($id = 1; $id <= $MAX_ID; $id++)
+{
+  // // Read in a MCI doctor page
+    $html = scraperwiki::scrape("http://www.mciindia.org/ViewDetails.aspx?ID=".$id);
+  // Find something on the page using css selectors
+   $dom = new simple_html_dom();
+   $dom->load($html);
+   
+   // walk through the dom and extract doctor information
+   echo doc_name = $dom->find('span[id=Name]')->plaintext;
+   doc_fname = $dom->find('span[id="FatherName"]')->plaintext;
+   doc_dob = $dom->find('span[id="DOB"]')->plaintext;
+   doc_infoyear = $dom->find('span[id="lbl_Info"]')->plaintext;
+   doc_regnum = $dom->find('span[id="Regis_no"]')->plaintext;
+   doc_datereg = $dom->find('span[id="Date_Reg"]')->plaintext;
+   doc_council = $dom->find('span[id="Lbl_Council"]')->plaintext;
+   doc_qual = $dom->find('span[id="Qual"]')->plaintext;
+   doc_qualyear = $dom->find('span[id="QualYear"]')->plaintext;
+   doc_univ = $dom->find('span[id="Univ"]')->plaintext;
+   doc_address = $dom->find('span[id="Address"]')->plaintext;
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
 
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
-?>
+
+}
