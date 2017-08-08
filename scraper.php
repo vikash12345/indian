35445c58482cc060249fb8810a93a2b3d3cf2326
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-08 11:40:31 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -29,10 +29,9 @@ for($id = 1; $id <= $MAX_ID; $id++)
 //
 
 	
-    if(is_object($Name)){
-	   	 		scraperwiki::save_sqlite(array('name'), array('name' => $Name , 'FatherName' => $FatherName,  'YOI' => $YOI,'DOB' => $DOB, 'RegNo' => $RegNo, 'DateReg' => $DateReg, 'SMC' => $SMC, 'Qual' => $Qual, 'QualYear' => $QualYear, 'Univ' => $Univ, 'Address' => $Address, 'url' => $url));
+   
 
-}
+
  
 
 // scraperwiki::select("* from data where 'name'='peter'")
@@ -42,7 +41,8 @@ for($id = 1; $id <= $MAX_ID; $id++)
 // called "data.sqlite" in the current working directory which has at least a table
 // called "data".
 }
- 
+ 	   	 		scraperwiki::save_sqlite(array('name'), array('name' => $Name , 'FatherName' => $FatherName,  'YOI' => $YOI,'DOB' => $DOB, 'RegNo' => $RegNo, 'DateReg' => $DateReg, 'SMC' => $SMC, 'Qual' => $Qual, 'QualYear' => $QualYear, 'Univ' => $Univ, 'Address' => $Address, 'url' => $url));
+
 
 
 ?>
