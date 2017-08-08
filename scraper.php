c08e0683e6e444ff14764eb13c636a80ac969f52
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-08 11:14:34 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -22,7 +22,7 @@ for($id = 1; $id <= $MAX_ID; $id++)
    echo $YOI			= $link->find('span[id="lbl_Info"]',0)->plaintext;
    echo $RegNo 			= $link->find('span[id="Regis_no"]',0)->plaintext;
    echo $DateReg		= $link->find('span[id="Date_Reg"]',0)->plaintext;
-   echo $SMC		= $link->find('span[id="Lbl_Council"]',0)->plaintext;
+   echo $SMC			= $link->find('span[id="Lbl_Council"]',0)->plaintext;
    echo $Qual			= $link->find('span[id="Qual"]',0)->plaintext;
    echo $QualYear		= $link->find('span[id="QualYear"]',0)->plaintext;
    echo $Univ			= $link->find('span[id="Univ"]',0)->plaintext;
@@ -38,7 +38,7 @@ for($id = 1; $id <= $MAX_ID; $id++)
 // called "data.sqlite" in the current working directory which has at least a table
 // called "data".
 }
-if($Name){
+if($Name != "" || $Name != "null"){
   		scraperwiki::save_sqlite(array('name'), array('name' => $Name , 'FatherName' => $FatherName,  'YOI' => $YOI,'DOB' => $DOB, 'RegNo' => $RegNo, 'DateReg' => $DateReg, 'SMC' => $SMC, 'Qual' => $Qual, 'QualYear' => $QualYear, 'Univ' => $Univ, 'Address' => $Address, 'url' => $url));
 
 }
