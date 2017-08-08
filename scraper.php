9542de3c7d78f47e911083dd75da59195d377228
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-08 15:10:05 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,13 +2,13 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 //
-
+$url1 = "https://old.mciindia.org/ViewDetails.aspx?ID=";
 $MAX_ID = 3; //set based on required maximum numbers
 /** looping over list of ids of doctors **/
 for($id = 1; $id <= $MAX_ID; $id++)
 {
   // // Read in a MCI doctor page
-    $url = ("https://old.mciindia.org/ViewDetails.aspx?ID=".$id);
+    $url = $url1 . $id;
   // Find something on the page using css selectors
 		$html = file_get_html($url);
 	
@@ -22,13 +22,13 @@ for($id = 1; $id <= $MAX_ID; $id++)
     $DOB			= $link->find('span[id="DOB"]',0)->plaintext;
     $YOI			= $link->find('span[id="lbl_Info"]',0)->plaintext;
     $RegNo 			= $link->find('span[id="Regis_no"]',0)->plaintext;
-    $DateReg		= $link->find('span[id="Date_Reg"]',0)->plaintext;
+    $DateReg			= $link->find('span[id="Date_Reg"]',0)->plaintext;
     $SMC			= $link->find('span[id="Lbl_Council"]',0)->plaintext;
     $Qual			= $link->find('span[id="Qual"]',0)->plaintext;
-    $QualYear		= $link->find('span[id="QualYear"]',0)->plaintext;
+    $QualYear			= $link->find('span[id="QualYear"]',0)->plaintext;
     $Univ			= $link->find('span[id="Univ"]',0)->plaintext;
-    $Address		= $link->find('span[id="Address"]',0)->plaintext;
-	sleep(5);
+    $Address			= $link->find('span[id="Address"]',0)->plaintext;
+
 	
 
 		scraperwiki::save_sqlite(array('name'), array('name' => $Name , 'FatherName' => $FatherName,  'YOI' => $YOI,'DOB' => $DOB, 'RegNo' => $RegNo, 'DateReg' => $DateReg, 'SMC' => $SMC, 'Qual' => $Qual, 'QualYear' => $QualYear, 'Univ' => $Univ, 'Address' => $Address, 'url' => $url));
