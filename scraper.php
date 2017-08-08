c27b32a6c2448a97cbc9d9264d4aa742b7b47051
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-08 12:02:35 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -12,28 +12,26 @@ for($id = 1; $id <= $MAX_ID; $id++)
   // Find something on the page using css selectors
 		$link = file_get_html($url);
    // walk through the dom and extract doctor information
-	
-     $info['Name']			= $link->find('span[id=Name]',0)->plaintext;
-    $info['FatherName']			= $link->find('span[id="FatherName"]',0)->plaintext;
-    $info['DOB']			= $link->find('span[id="DOB"]',0)->plaintext;
-    $info['YOI']			= $link->find('span[id="lbl_Info"]',0)->plaintext;
-   $info['RegNo'] 			= $link->find('span[id="Regis_no"]',0)->plaintext;
-   $info['DateReg']			= $link->find('span[id="Date_Reg"]',0)->plaintext;
-   $info['SMC']			= $link->find('span[id="Lbl_Council"]',0)->plaintext;
-   $info['Qual']		= $link->find('span[id="Qual"]',0)->plaintext;
-   $info['QualYear']			= $link->find('span[id="QualYear"]',0)->plaintext;
-   $info['Univ']			= $link->find('span[id="Univ"]',0)->plaintext;
-   $info['Address']		= $link->find('span[id="Address"]',0)->plaintext;
-	if(is_object($info['Name'])){
-    scraperwiki::save_sqlite(array('name'), array('name' => $info['Name'] , 'FatherName' => $info['FatherName'],  'YOI' => $info['YOI'], 'DOB' => $info['DOB'], 'RegNo' => $info['RegNo'], 'DateReg' => $info['DateReg'], 'SMC' => $info['SMC'], 'Qual' => $info['Qual'], 'QualYear' => $info['QualYear'], 'Univ' => $info['Univ'], 'Address' => $info['Address'], 'url' => $info['url']));
-	
-	}
+   if($link){
+   echo $Name 			= $link->find('span[id=Name]',0)->plaintext;
+   echo $FatherName 		= $link->find('span[id="FatherName"]',0)->plaintext;
+   echo $DOB			= $link->find('span[id="DOB"]',0)->plaintext;
+   echo $YOI			= $link->find('span[id="lbl_Info"]',0)->plaintext;
+   echo $RegNo 			= $link->find('span[id="Regis_no"]',0)->plaintext;
+   echo $DateReg		= $link->find('span[id="Date_Reg"]',0)->plaintext;
+   echo $SMC			= $link->find('span[id="Lbl_Council"]',0)->plaintext;
+   echo $Qual			= $link->find('span[id="Qual"]',0)->plaintext;
+   echo $QualYear		= $link->find('span[id="QualYear"]',0)->plaintext;
+   echo $Univ			= $link->find('span[id="Univ"]',0)->plaintext;
+   echo $Address		= $link->find('span[id="Address"]',0)->plaintext;
+	 scraperwiki::save_sqlite(array('name'), array('name' => $Name , 'FatherName' => $FatherName,  'YOI' => $YOI,'DOB' => $DOB, 'RegNo' => $RegNo, 'DateReg' => $DateReg, 'SMC' => $SMC, 'Qual' => $Qual, 'QualYear' => $QualYear, 'Univ' => $Univ, 'Address' => $Address, 'url' => $url));
+
+}
+   }
 //
 
 	
    
-
-
  
 
 // scraperwiki::select("* from data where 'name'='peter'")
@@ -42,7 +40,7 @@ for($id = 1; $id <= $MAX_ID; $id++)
 // All that matters is that your final data is written to an SQLite database
 // called "data.sqlite" in the current working directory which has at least a table
 // called "data".
-}
+
 
 
 
