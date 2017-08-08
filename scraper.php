129589c0f661176f5becfaa7e2180d88b617c199
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-08 14:28:48 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -12,23 +12,19 @@ for($id = 1; $id <= $MAX_ID; $id++)
   // Find something on the page using css selectors
 		$link = file_get_html($url);
    // walk through the dom and extract doctor information
-      echo $Name 			= $link->find('span[id=Name]',0)->plaintext;
-   echo $FatherName 		= $link->find('span[id="FatherName"]',0)->plaintext;
-   echo $DOB			= $link->find('span[id="DOB"]',0)->plaintext;
-   echo $YOI			= $link->find('span[id="lbl_Info"]',0)->plaintext;
-   echo $RegNo 			= $link->find('span[id="Regis_no"]',0)->plaintext;
-   echo $DateReg		= $link->find('span[id="Date_Reg"]',0)->plaintext;
-   echo $SMC			= $link->find('span[id="Lbl_Council"]',0)->plaintext;
-   echo $Qual			= $link->find('span[id="Qual"]',0)->plaintext;
-   echo $QualYear		= $link->find('span[id="QualYear"]',0)->plaintext;
-   echo $Univ			= $link->find('span[id="Univ"]',0)->plaintext;
-   echo $Address		= $link->find('span[id="Address"]',0)->plaintext;
+     $Name 			= $link->find('span[id=Name]',0)->plaintext;
+    $FatherName 		= $link->find('span[id="FatherName"]',0)->plaintext;
+    $DOB			= $link->find('span[id="DOB"]',0)->plaintext;
+    $YOI			= $link->find('span[id="lbl_Info"]',0)->plaintext;
+    $RegNo 			= $link->find('span[id="Regis_no"]',0)->plaintext;
+    $DateReg		= $link->find('span[id="Date_Reg"]',0)->plaintext;
+    $SMC			= $link->find('span[id="Lbl_Council"]',0)->plaintext;
+    $Qual			= $link->find('span[id="Qual"]',0)->plaintext;
+    $QualYear		= $link->find('span[id="QualYear"]',0)->plaintext;
+    $Univ			= $link->find('span[id="Univ"]',0)->plaintext;
+    $Address		= $link->find('span[id="Address"]',0)->plaintext;
 	
-	if($Name != null)
-	{
-		scraperwiki::save_sqlite(array('name'), array('name' => $Name , 'FatherName' => $FatherName,  'YOI' => $YOI,'DOB' => $DOB, 'RegNo' => $RegNo, 'DateReg' => $DateReg, 'SMC' => $SMC, 'Qual' => $Qual, 'QualYear' => $QualYear, 'Univ' => $Univ, 'Address' => $Address, 'url' => $url));
-
-	}
+	
 
 
  
@@ -45,7 +41,11 @@ for($id = 1; $id <= $MAX_ID; $id++)
 // called "data.sqlite" in the current working directory which has at least a table
 // called "data".
 }
+if($Name != null)
+	{
+		scraperwiki::save_sqlite(array('name'), array('name' => $Name , 'FatherName' => $FatherName,  'YOI' => $YOI,'DOB' => $DOB, 'RegNo' => $RegNo, 'DateReg' => $DateReg, 'SMC' => $SMC, 'Qual' => $Qual, 'QualYear' => $QualYear, 'Univ' => $Univ, 'Address' => $Address, 'url' => $url));
 
+	}
 
 
 ?>
