ce038b7fe17f8e6df8acce4fb68276a0b33cc68f
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-08 15:57:56 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -6,28 +6,29 @@ require 'scraperwiki/simple_html_dom.php';
 for($id = 1; $id <= 3; $id++)
 	{
     		$url = ("https://old.mciindia.org/ViewDetails.aspx?ID=".$id);
-		
-	}
-
-	$link = file_get_html($url);
+		$link = file_get_html($url);
    // walk through the dom and extract doctor information
 	if($link){
-    $Name 			= $link->find('span[id=Name]',0)->plaintext;
-    $FatherName 		= $link->find('span[id="FatherName"]',0)->plaintext;
-    $DOB			= $link->find('span[id="DOB"]',0)->plaintext;
-    $YOI			= $link->find('span[id="lbl_Info"]',0)->plaintext;
-    $RegNo 			= $link->find('span[id="Regis_no"]',0)->plaintext;
-    $DateReg			= $link->find('span[id="Date_Reg"]',0)->plaintext;
-    $SMC			= $link->find('span[id="Lbl_Council"]',0)->plaintext;
-    $Qual			= $link->find('span[id="Qual"]',0)->plaintext;
-    $QualYear			= $link->find('span[id="QualYear"]',0)->plaintext;
-    $Univ			= $link->find('span[id="Univ"]',0)->plaintext;
-    $Address			= $link->find('span[id="Address"]',0)->plaintext;
+   		 $Name 			= $link->find('span[id=Name]',0)->plaintext;
+    		$FatherName 		= $link->find('span[id="FatherName"]',0)->plaintext;
+   		 $DOB			= $link->find('span[id="DOB"]',0)->plaintext;
+    		$YOI			= $link->find('span[id="lbl_Info"]',0)->plaintext;
+   		 $RegNo 			= $link->find('span[id="Regis_no"]',0)->plaintext;
+    		$DateReg			= $link->find('span[id="Date_Reg"]',0)->plaintext;
+		    $SMC			= $link->find('span[id="Lbl_Council"]',0)->plaintext;
+    		$Qual			= $link->find('span[id="Qual"]',0)->plaintext;
+    		$QualYear			= $link->find('span[id="QualYear"]',0)->plaintext;
+   		 $Univ			= $link->find('span[id="Univ"]',0)->plaintext;
+   		 $Address			= $link->find('span[id="Address"]',0)->plaintext;
 	}
 if($Name){
 	echo $Name;
     scraperwiki::save_sqlite(array('name'), array('name' => $Name , 'FatherName' => $FatherName,  'YOI' => $YOI,'DOB' => $DOB, 'RegNo' => $RegNo, 'DateReg' => $DateReg, 'SMC' => $SMC, 'Qual' => $Qual, 'QualYear' => $QualYear, 'Univ' => $Univ, 'Address' => $Address, 'url' => $url));
 }
+		
+	}
+
+	
 //
 	
 	
