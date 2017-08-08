39c953fdd1156bf4de58512bdba06983b57acc82
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-08 11:45:13 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,7 +3,7 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 //
 
-$MAX_ID = 1; //set based on required maximum numbers
+$MAX_ID = 3; //set based on required maximum numbers
 /** looping over list of ids of doctors **/
 for($id = 1; $id <= $MAX_ID; $id++)
 {
@@ -13,7 +13,7 @@ for($id = 1; $id <= $MAX_ID; $id++)
 		$link = file_get_html($url);
    // walk through the dom and extract doctor information
    
-    echo $Name 			= $link->find('span[id=Name]',0)->plaintext;
+    $Name 			= $link->find('span[id=Name]',0)->plaintext;
     $FatherName 		= $link->find('span[id="FatherName"]',0)->plaintext;
     $DOB			= $link->find('span[id="DOB"]',0)->plaintext;
     $YOI			= $link->find('span[id="lbl_Info"]',0)->plaintext;
@@ -24,8 +24,9 @@ for($id = 1; $id <= $MAX_ID; $id++)
     $QualYear		= $link->find('span[id="QualYear"]',0)->plaintext;
     $Univ			= $link->find('span[id="Univ"]',0)->plaintext;
     $Address		= $link->find('span[id="Address"]',0)->plaintext;
+	if($link == true){
     scraperwiki::save_sqlite(array('name'), array('name' => $Name , 'FatherName' => $FatherName,  'YOI' => $YOI,'DOB' => $DOB, 'RegNo' => $RegNo, 'DateReg' => $DateReg, 'SMC' => $SMC, 'Qual' => $Qual, 'QualYear' => $QualYear, 'Univ' => $Univ, 'Address' => $Address, 'url' => $url));
-
+	}
    
 //
 
