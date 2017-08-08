4e47797f4c426b1e6430f8913df2e197d89180c7
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-08 11:57:25 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -12,18 +12,18 @@ for($id = 1; $id <= $MAX_ID; $id++)
   // Find something on the page using css selectors
 		$link = file_get_html($url);
    // walk through the dom and extract doctor information
-   
-    $info['$Name']			= $link->find('span[id=Name]',0)->plaintext;
-    $info['$FatherName']		= $link->find('span[id="FatherName"]',0)->plaintext;
-    $info['$DOB']			= $link->find('span[id="DOB"]',0)->plaintext;
-    $info['$YOI']			= $link->find('span[id="lbl_Info"]',0)->plaintext;
-   $info['$RegNo'] 			= $link->find('span[id="Regis_no"]',0)->plaintext;
-   $info['$DateReg']			= $link->find('span[id="Date_Reg"]',0)->plaintext;
-   $info['$SMC']			= $link->find('span[id="Lbl_Council"]',0)->plaintext;
-   $info['$Qual']		= $link->find('span[id="Qual"]',0)->plaintext;
-   $info['$QualYear']			= $link->find('span[id="QualYear"]',0)->plaintext;
-   $info['$Univ']			= $link->find('span[id="Univ"]',0)->plaintext;
-   $info['$Address']		= $link->find('span[id="Address"]',0)->plaintext;
+	
+     $info['Name']			= $link->find('span[id=Name]',0)->plaintext;
+    $info['FatherName']		= $link->find('span[id="FatherName"]',0)->plaintext;
+    $info['DOB']			= $link->find('span[id="DOB"]',0)->plaintext;
+    $info['YOI']			= $link->find('span[id="lbl_Info"]',0)->plaintext;
+   $info['RegNo'] 			= $link->find('span[id="Regis_no"]',0)->plaintext;
+   $info['DateReg']			= $link->find('span[id="Date_Reg"]',0)->plaintext;
+   $info['SMC']			= $link->find('span[id="Lbl_Council"]',0)->plaintext;
+   $info['Qual']		= $link->find('span[id="Qual"]',0)->plaintext;
+   $info['QualYear']			= $link->find('span[id="QualYear"]',0)->plaintext;
+   $info['Univ']			= $link->find('span[id="Univ"]',0)->plaintext;
+   $info['Address']		= $link->find('span[id="Address"]',0)->plaintext;
 	
     scraperwiki::save_sqlite(array('name'), array('name' => $info['$Name'] , 'FatherName' => $info['$FatherName'],  'YOI' => $info['$YOI'], 'DOB' => $info['$DOB'], 'RegNo' => $info['$RegNo'], 'DateReg' => $info['$DateReg'], 'SMC' => $info['$SMC'], 'Qual' => $info['$Qual'], 'QualYear' => $info['$QualYear'], 'Univ' => $info['$Univ'], 'Address' => $info['$Address'], 'url' => $info['$url']));
 	
