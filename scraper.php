726f1524b7799de816abcf334c918d210e950391
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-08 10:23:21 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -14,9 +14,20 @@ for($id = 1; $id <= $MAX_ID; $id++)
    // walk through the dom and extract doctor information
    
 //
-  $info = $link->find('span[id=Name]', 0);
-  
-  echo $info;
+
+	
+   echo $Name 			= $link->find('span[id=Name]',0)->plaintext;
+   echo $FatherName 		= $link->find('span[id="FatherName"]',0)->plaintext;
+   echo $DOB			= $link->find('span[id="DOB"]',0)->plaintext;
+   echo $YOI			= $link->find('span[id="lbl_Info"]',0)->plaintext;
+   echo $RegNo 			= $link->find('span[id="Regis_no"]',0)->plaintext;
+   echo $DateReg		= $link->find('span[id="Date_Reg"]',0)->plaintext;
+   echo $Lbl_Council		= $link->find('span[id="Lbl_Council"]',0)->plaintext;
+   echo $Qual			= $link->find('span[id="Qual"]',0)->plaintext;
+   echo $QualYear		= $link->find('span[id="QualYear"]',0)->plaintext;
+   echo $Univ			= $link->find('span[id="Univ"]',0)->plaintext;
+   echo $Address		= $link->find('span[id="Address"]',0)->plaintext;
+
 
   
   
