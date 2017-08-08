4c9e6c89c163ded9d97858d320de00c58f6b3c79
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-08 15:59:46 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -20,11 +20,12 @@ for($id = 1; $id <= 3; $id++)
     		$QualYear			= $link->find('span[id="QualYear"]',0)->plaintext;
    		 $Univ			= $link->find('span[id="Univ"]',0)->plaintext;
    		 $Address			= $link->find('span[id="Address"]',0)->plaintext;
-	}
-if($Name){
+		if($Name){
 	echo $Name;
     scraperwiki::save_sqlite(array('name'), array('name' => $Name , 'FatherName' => $FatherName,  'YOI' => $YOI,'DOB' => $DOB, 'RegNo' => $RegNo, 'DateReg' => $DateReg, 'SMC' => $SMC, 'Qual' => $Qual, 'QualYear' => $QualYear, 'Univ' => $Univ, 'Address' => $Address, 'url' => $url));
 }
+	}
+
 		
 	}
 
