6f326ec0f17f66e2e1685e101096203fde7196ed
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-08 10:06:04 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -2,18 +2,30 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 //
-$MAX_ID = 3; //set based on required maximum numbers
+
+$MAX_ID = 1; //set based on required maximum numbers
 /** looping over list of ids of doctors **/
-for($id = 1; $id <= 1; $id++)
+for($id = 1; $id <= $MAX_ID; $id++)
 {
   // // Read in a MCI doctor page
-    $html = scraperwiki::scrape("https://old.mciindia.org/ViewDetails.aspx?ID=".$id);
+    $url = ("https://old.mciindia.org/ViewDetails.aspx?ID=".$id);
   // Find something on the page using css selectors
-   $dom = new simple_html_dom();
-   $dom->load($html);
-   foreach($dom->find("//*[@id='form1']/div[3]/table/tbody") as $element)
-{
-	echo $element;
-   }
+		$link = file_get_html($url);
+   // walk through the dom and extract doctor information
+   
+//
+  $info = $link->find('span[id=Name]', 0);
+  
+  echo $info;
+
+  
+  
+
+// scraperwiki::select("* from data where 'name'='peter'")
+// You don't have to do things with the ScraperWiki library.
+// You can use whatever libraries you want: https://morph.io/documentation/php
+// All that matters is that your final data is written to an SQLite database
+// called "data.sqlite" in the current working directory which has at least a table
+// called "data".
 }
 ?>
