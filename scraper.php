673f48dc32eb024f5a2458f35147bc031f577faf
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-08 11:37:58 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -14,16 +14,16 @@ for($id = 1; $id <= $MAX_ID; $id++)
    // walk through the dom and extract doctor information
    if($link){
    echo $Name 			= $link->find('span[id=Name]',0)->plaintext;
-   echo $FatherName 		= $link->find('span[id="FatherName"]',0)->plaintext;
-   echo $DOB			= $link->find('span[id="DOB"]',0)->plaintext;
-   echo $YOI			= $link->find('span[id="lbl_Info"]',0)->plaintext;
-   echo $RegNo 			= $link->find('span[id="Regis_no"]',0)->plaintext;
-   echo $DateReg		= $link->find('span[id="Date_Reg"]',0)->plaintext;
-   echo $SMC			= $link->find('span[id="Lbl_Council"]',0)->plaintext;
-   echo $Qual			= $link->find('span[id="Qual"]',0)->plaintext;
-   echo $QualYear		= $link->find('span[id="QualYear"]',0)->plaintext;
-   echo $Univ			= $link->find('span[id="Univ"]',0)->plaintext;
-   echo $Address		= $link->find('span[id="Address"]',0)->plaintext;
+    $FatherName 		= $link->find('span[id="FatherName"]',0)->plaintext;
+    $DOB			= $link->find('span[id="DOB"]',0)->plaintext;
+    $YOI			= $link->find('span[id="lbl_Info"]',0)->plaintext;
+    $RegNo 			= $link->find('span[id="Regis_no"]',0)->plaintext;
+    $DateReg		= $link->find('span[id="Date_Reg"]',0)->plaintext;
+    $SMC			= $link->find('span[id="Lbl_Council"]',0)->plaintext;
+    $Qual			= $link->find('span[id="Qual"]',0)->plaintext;
+    $QualYear		= $link->find('span[id="QualYear"]',0)->plaintext;
+    $Univ			= $link->find('span[id="Univ"]',0)->plaintext;
+    $Address		= $link->find('span[id="Address"]',0)->plaintext;
 	   if(is_object($Name)){
 	   	 		scraperwiki::save_sqlite(array('name'), array('name' => $Name , 'FatherName' => $FatherName,  'YOI' => $YOI,'DOB' => $DOB, 'RegNo' => $RegNo, 'DateReg' => $DateReg, 'SMC' => $SMC, 'Qual' => $Qual, 'QualYear' => $QualYear, 'Univ' => $Univ, 'Address' => $Address, 'url' => $url));
 
