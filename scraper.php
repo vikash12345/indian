4cd094ca203a603b8249cc1b63528e6392db2ca5
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-08 11:08:47 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -28,10 +28,10 @@ for($id = 1; $id <= $MAX_ID; $id++)
    echo $Univ			= $link->find('span[id="Univ"]',0)->plaintext;
    echo $Address		= $link->find('span[id="Address"]',0)->plaintext;
 
-
+if($Name){
   		scraperwiki::save_sqlite(array('name'), array('name' => $Name , 'FatherName' => $FatherName,  'YOI' => $YOI,'DOB' => $DOB, 'RegNo' => $RegNo, 'DateReg' => $DateReg, 'SMC' => $SMC, 'Qual' => $Qual, 'QualYear' => $QualYear, 'Univ' => $Univ, 'Address' => $Address, 'url' => $url));
 
-  
+}
 
 // scraperwiki::select("* from data where 'name'='peter'")
 // You don't have to do things with the ScraperWiki library.
