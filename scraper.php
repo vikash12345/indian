93d91277ea86e49807773948e940114246378fe5
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-10-02 09:08:33 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,7 +3,7 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 //
 /** looping over list of ids of doctors **/
-for($id = 1094060; $id <= 1199999; $id++)
+for($id = 1128965; $id <= 1199999; $id++)
 	{
 
 	 $url = ("https://old.mciindia.org/ViewDetails.aspx?ID=".$id);
